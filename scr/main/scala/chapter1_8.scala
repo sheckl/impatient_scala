8af1f98cea8dd424161e8187bf2c7cf165855afc
she <Maxially87>
2013-11-21 14:26:28 +0100

    chapter 1 exercise 8

--- scr/main/scala/chapter1_8.scala
+++ scr/main/scala/chapter1_8.scala
@@ -1,4 +1,10 @@
-//println(BigInt(100, scala.util.Random).toString(36))
+// Ausfuehren mit: scala scr/main/scala/chapter1_8.scala (erst in den Ordner impatien_scala gehen)
 
-val random = BigInt(100, scala.util.Random)
+// Hier wird die Anzahl der Bits durch den Benutzer in der Shell eingegeben (z.B. 100)
+print("> ")
+val bits = readInt()
+val random = BigInt(bits, scala.util.Random)
 println(random.toString(36))
+
+
+//println(BigInt(100, scala.util.Random).toString(36))
