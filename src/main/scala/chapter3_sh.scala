0758d4c508bcfeeae35839454acd447922d572ac
she <Maxially87>
2013-11-27 09:08:20 +0100

    new version of exercises 2 and 3 chapter 3

--- src/main/scala/chapter3_sh.scala
+++ src/main/scala/chapter3_sh.scala
@@ -17,27 +17,35 @@ val n = 10
 val randoms = for (i <- 1 until n) yield Random.nextInt(n)
 println(randoms.toArray.mkString(", "))
 
+
 // Exercise 2
 
 val b = Array(1, 2, 3, 4, 5)
 
-for (i <- 0 until (b.length,2)) {
-  if (i < b.length - 1) {
-    var arraySlice = b.slice(i, i + 2).reverse
-    b(i) = arraySlice(0)
-    b(i + 1) = arraySlice(1)
+for (i <- 0 until b.length) {
+  if (i % 2 == 0 & i < b.length - 1) {
+    var firstNumber = b(i)
+    b(i) = b(i + 1)
+    b(i + 1) = firstNumber
   }
   b
 }
 
 println(b.mkString(", "))
 
+
 // Exercise 3
 
+val c = Array(1, 2, 3, 4, 5)
+
+val newArray = for (i <- 0 until c.length) yield {
+  if (i % 2 == 0 & i == c.length - 1) c(i)
+  else if (i % 2 == 0) c(i + 1)
+  else c(i - 1)
+}
+
+println(newArray.mkString(", "))
 
 
-// Exercise 4
 
-val d = Array(2, 3, -5, 6, -8, 1)
 
-//val indexes_positive = for (i <- 0 until d.length) yield
