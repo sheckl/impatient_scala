ac425f74252e61e7d03a701a01b28ce09059fa15
she <Maxially87>
2013-12-05 14:54:34 +0100

    chapter 5 exercise 1 add spaces

--- chapter5_1.scala
+++ chapter5_1.scala
@@ -1,10 +1,12 @@
 
 class Counter {
   private var value = Int.MaxValue - 1
+
   def increment() {
     if (value < Int.MaxValue) value += 1 else
     throw new IllegalStateException("x is bigger than Int.MaxValue")
   }
+
   def current() = value
 }
 
