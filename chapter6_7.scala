56cdf64e0dddd156ea3535f7741d05e18a359f05
she <Maxially87>
2013-12-11 11:13:47 +0100

    chapter 6 exercise 7

---
+++ chapter6_7.scala
@@ -0,0 +1,11 @@
+
+object cardSuits extends Enumeration {
+  val Kreuz = Value("♣")
+  val Karo = Value("♦")
+  val Herz = Value("♥")
+  val Pik = Value("♠")
+  def isRed(cardSuit: Value) = { cardSuit == Karo || cardSuit == Herz }
+}
+
+for (c <- cardSuits.values) println(c.id + ": " + c)
+for (c <- cardSuits.values) println(c + " is red: " + cardSuits.isRed(c))
