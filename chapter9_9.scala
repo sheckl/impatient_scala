e9d537eb1e3a028d5291e16203031e210b204fdb
she <Maxially87>
2014-01-31 08:50:42 +0100

    chapter 9 exercise 9 add refactored solution

--- chapter9_9.scala
+++ chapter9_9.scala
@@ -15,4 +15,23 @@ def foo(dir: File) {
 
 foo(new File("."))
 
-//hier bietet sich "refactor -> extract -> method" an (-> Mail von Oli)
+//hier bietet sich "refactor -> extract -> method" an
+
+//Lösung Alex (nach refactor):
+/*
+import java.io.File
+
+def isScalaSourceFile(child: File): Boolean = {
+  child.getName.matches( """[a-z0-9_]+.scala""")
+}
+
+def foo(dir: File) {
+  val contents = dir.listFiles
+
+  contents.filter(f => f.isFile && isScalaSourceFile(f)).foreach(f => println(f.getAbsolutePath))
+
+  contents.filter(_.isDirectory).foreach(foo)
+}
+
+foo(new File(".."))
+*/
