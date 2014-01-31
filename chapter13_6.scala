505580c2f7d334593cdab475b4c46da97c7c29b5
she <Maxially87>
2014-01-31 16:36:32 +0100

    chapter 10 exercise 6

--- chapter13_6.scala
+++ chapter13_6.scala
@@ -2,7 +2,9 @@
 val lst = List(1,2,3,4,5)
 
 println((lst :\ List[Int]())(_ :: _))
-
 println((List[Int]() /: lst)(_ :+ _))
+// hier werden die einzelnen Elemente von lst aneinander gereiht (einmal mit :: und einmal mit :+)
 
-println((List[Int]() /: lst)((x,y) => y::x))
+println((List[Int]() /: lst)((x,y) => y +: x))
+// hier wird die 'alte' Liste an das neue Element gehängt,
+// wodurch sich die Liste 'umdreht'
